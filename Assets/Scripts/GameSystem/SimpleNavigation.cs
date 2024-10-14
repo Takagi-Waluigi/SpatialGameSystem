f355ea9d724fe0b6e8885b82c51d88adb49c98bb
Takagi-Kenta <tokage.kentacky420@gmail.com>
2024-10-14 22:45:51 +0900

    Final Commit in 10/14

--- Assets/Scripts/GameSystem/SimpleNavigation.cs
+++ Assets/Scripts/GameSystem/SimpleNavigation.cs
@@ -7,13 +7,6 @@ public class SimpleNavigation : MonoBehaviour
 {
     [SerializeField] Transform targetTransform;
     [SerializeField] NavMeshAgent agent;
-    [SerializeField] GameObject enemyRootObject;
-    [SerializeField] float freezingTime = 3f;
-    bool isFreezing = false;
-    [SerializeField] float freezingAreaMax = 0.5f;
-    Vector3 basePosition;
-    float beginTime = 0f;
-
 
     // Start is called before the first frame update
     void Start()
@@ -23,31 +16,6 @@ public class SimpleNavigation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if(this.gameObject.GetComponent<StateManager>().isAttacked)
-        {
-            beginTime = Time.time;
-            basePosition = this.transform.position;
-            isFreezing = true;
-        }
-
-        this.gameObject.GetComponent<NavMeshAgent>().enabled = !isFreezing;
-
-        if(isFreezing)
-        {
-            if(Time.time - beginTime > freezingTime) isFreezing = false;
-
-            var freezingPosition = new Vector3(
-                basePosition.x + Random.Range(-freezingAreaMax * 0.5f, freezingAreaMax * 0.5f),
-                basePosition.y,
-                basePosition.z + Random.Range(-freezingAreaMax * 0.5f, freezingAreaMax * 0.5f)
-            );
-
-            this.transform.position = freezingPosition;
-        }
-        else
-        {            
-            agent.SetDestination(targetTransform.position);
-        }
+        agent.SetDestination(targetTransform.position);
     }
 }
