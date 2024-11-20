83d82022aff99b714425e32d58fb62e9d001c583
Takagi-Waluigi <tokage.kentacky420@gmail.com>
2024-11-20 23:39:55 +0900

    add sudio map

--- Assets/Scripts/GameSystem/CardRandomLayout.cs
+++ Assets/Scripts/GameSystem/CardRandomLayout.cs
@@ -1,67 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class CardRandomLayout : MonoBehaviour
 {
-    [SerializeField] List<GameObject> cardObjects = new List<GameObject>();
-    [SerializeField] Transform baseTransform;
-    [SerializeField] float xRange = 3f;
-    [SerializeField] float zRange = 2f;
-    [SerializeField] int maxAttempt = 100;
+    [SerializeField] StateManager stateManager;
+    [SerializeField] List<GameObject> layoutedCardObjects = new List<GameObject>();
+    int index = 0;
+    bool lastIsGameOver = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        cardSpawner();
     }
-
-    void cardSpawner()
-    {
-        foreach(GameObject card in cardObjects)
-        {
-            card.transform.position = new Vector3(0f, 10f, 0f);
-        }
-
-        foreach(GameObject card in cardObjects)
-        {
-            bool isSucceeded = false;
-            int numAttempt = 0;
-
-            while(!isSucceeded)
-            {
-                numAttempt ++;
-
-                Vector3 randomPosition = new Vector3(
-                baseTransform.position.x + Random.Range(-xRange * 0.5f, xRange * 0.5f),
-                0.01f,
-                baseTransform.position.z + Random.Range(-zRange * 0.5f, zRange * 0.5f)
-                );
-
-                //Debug.Log("attempt:" + randomPosition);
-
-                float randomAngle = Random.Range(-180f, 180f);
-
-                if(!Physics.CheckSphere(randomPosition, 2f))
-                {
-                    card.transform.position = randomPosition;
-                    card.transform.rotation = Quaternion.Euler(0f, randomAngle, 0f);
-
-                    isSucceeded = true;
-                }
-
-                if(numAttempt > maxAttempt)
-                {
-                    Debug.LogWarning("Over Max Atempt");
-                    isSucceeded = true;
-                }
-            }            
-        }
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!stateManager.isGameOver && lastIsGameOver)
+        {
+            if(layoutedCardObjects.Count > 0)
+            {
+                index = (index + 1) % layoutedCardObjects.Count;
+                
+                for(int i = 0; i < this.transform.childCount; i ++)
+                {
+                    Transform child = this.transform.GetChild(i);
+                    child.transform.position = layoutedCardObjects[index].transform.GetChild(i).position;
+                    child.transform.rotation = layoutedCardObjects[index].transform.GetChild(i).rotation;
+                }
+            }
+        }
+
+        lastIsGameOver = stateManager.isGameOver;
     }
 }
